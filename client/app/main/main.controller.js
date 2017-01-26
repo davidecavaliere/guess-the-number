99bd1e47abea6b406875a07ce87a268ce5e88981
Davide Cavaliere <cavaliere.davide@gmail.com>
2017-01-26 11:25:22 +0100

    scaffolding main controller

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,8 +2,16 @@
 
 angular.module('bazooKaApp')
 
-.controller('MainCtrl', function($scope, $log, User, Auth, documentService, $location) {
+.controller('MainCtrl', function($scope, $log, $mdToast) {
 
+  $scope.selectNumber = function(number) {
 
+    if ($scope.form.$valid) {
+      $log.debug('number selected', number);
+
+    } else {
+       $scope.form.$setSubmitted(true);
+    }
+  };
 
 });
