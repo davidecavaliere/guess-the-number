5faec667c1c5196c58b7e12c95c4a9ab1dbeb52b
Davide Cavaliere <cavaliere.davide@gmail.com>
2017-01-26 11:44:47 +0100

    adding login to check whether the guessed number is correct, show relative messages and calculate number of attemps left

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,12 +2,31 @@
 
 angular.module('bazooKaApp')
 
-.controller('MainCtrl', function($scope, $log, $mdToast) {
+.controller('MainCtrl', function($scope, $log, $mdToast, NumberService) {
+
+  var winningNumber = NumberService.getRandomNumber();
+  $log.debug('winning number is', winningNumber);
+
+  $scope.attemps = 3;
 
   $scope.selectNumber = function(number) {
 
     if ($scope.form.$valid) {
       $log.debug('number selected', number);
+
+      if (number == winningNumber) {
+        $scope.winnerMessage = true;
+        $scope.tryAgainMessage = false;
+      } else {
+        $scope.tryAgainMessage = true;
+        $scope.attemps--;
+      }
+
+      if ($scope.attemps == 0) {
+        $scope.refreshMessage = true;
+        $scope.tryAgainMessage = false;
+
+      }
 
     } else {
        $scope.form.$setSubmitted(true);
