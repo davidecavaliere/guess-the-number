1b7366f65fd9702b598adc4e09b7cf75954cdfc3
Davide Cavaliere <cavaliere.davide@gmail.com>
2017-01-26 12:02:58 +0100

    adding handler to log numbers

--- client/components/number.service.js
+++ client/components/number.service.js
@@ -1,13 +1,22 @@
 angular.module('bazooKaApp')
-.service('NumberService', function() {
+.service('NumberService', function($resource) {
   var min = 1;
   var max = 100;
 
   var number = Math.floor(Math.random() * (max - min) + min);
 
+  var resource = $resource('/api/numbers/:id', { id : '@id' }, {
+    save : {
+      method : 'POST'
+    }
+  });
+
   return {
     getRandomNumber : function() {
       return number;
+    },
+    logNumber : function(number, callback, err) {
+      return resource.save(number, callback, err);
     }
   }
 });
