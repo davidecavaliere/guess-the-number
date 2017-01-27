8d3e9639ba016b2bb5b0a3242c4bb8524e04b9be
Davide Cavaliere <cavaliere.davide@gmail.com>
2017-01-27 17:15:28 +0100

    adding tests cases for app

--- e2e/main/main.po.js
+++ e2e/main/main.po.js
@@ -6,10 +6,15 @@
 'use strict';
 
 var MainPage = function() {
-  this.heroEl = element(by.css('.hero-unit'));
-  this.h1El = this.heroEl.element(by.css('h1'));
-  this.imgEl = this.heroEl.element(by.css('img'));
+  this.header = element(by.css('.md-toolbar-tools > h2 > span'));
+  this.title = element(by.css('body > md-content > md-content > h1'));
+  this.attemptsText = element(by.binding('attempts'));
+  this.checkButton = element(by.css('[type="submit"]'));
+  this.inputNumber = element(by.model('number'));
+  this.emptyMessage = element(by.css('[ng-message="required"]'));
+  this.minMessage = element(by.css('[ng-message="min"]'));
+  this.maxMessage = element(by.css('[ng-message="max"]'));
+
 };
 
 module.exports = new MainPage();
-
--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -8,9 +8,70 @@ describe('Main View', function() {
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  it('should have an header', function() {
+    // console.log(page.header.getText());
+    expect(page.header.getText()).toBe('Guess the number');
   });
+
+
+  it('should show a message with range number', function() {
+    // console.log(page.title);
+    expect(page.title.getText()).toBe('Please enter a number betwee 1 and 100');
+  });
+
+  it('should show a message with number of attemps', function() {
+    expect(page.attemptsText.getText()).toBe('You have 3 attempts left');
+  });
+
+  it('should have an input empty input box', function() {
+    // console.log(page.inputNumber);
+    expect(page.inputNumber.getText()).toBe('');
+  });
+
+  it('should give an error if input is empty', function() {
+    var input = page.inputNumber;
+    input.sendKeys('');
+    page.checkButton.click();
+    expect(page.emptyMessage.isDisplayed()).toBeTruthy();
+    expect(page.emptyMessage.getText()).toBe('You must input a number');
+  });
+
+  it('should give an error if input is less than 1', function() {
+    var input = page.inputNumber;
+    input.sendKeys('0');
+    page.checkButton.click();
+    expect(page.minMessage.isDisplayed()).toBeTruthy();
+    expect(page.minMessage.getText()).toBe('Number must be equal or greater than 1');
+  });
+
+  it('should give an error if input is greater than 100', function() {
+    var input = page.inputNumber;
+    input.sendKeys('101');
+    page.checkButton.click();
+    expect(page.maxMessage.isDisplayed()).toBeTruthy();
+    expect(page.maxMessage.getText()).toBe('Number must be lower or equal to 100');
+  });
+
+  it('should reduce the number of attempts from 3 to 0', function() {
+    var input = page.inputNumber;
+    input.sendKeys('12');
+    page.checkButton.click();
+    expect(page.attemptsText.getText()).toBe('You have 2 attempts left');
+
+    var input = page.inputNumber;
+    input.click().clear().sendKeys('13');
+    page.checkButton.click();
+    expect(page.attemptsText.getText()).toBe('You have 1 attempts left');
+
+    var input = page.inputNumber;
+    input.click().clear().sendKeys('14');
+    page.checkButton.click();
+    expect(page.attemptsText.getText()).toBe('You have 0 attempts left');
+
+    expect(page.inputNumber.isEnabled()).toBe(false);
+    expect(page.checkButton.isEnabled()).toBe(false);
+
+  });
+
+
 });
